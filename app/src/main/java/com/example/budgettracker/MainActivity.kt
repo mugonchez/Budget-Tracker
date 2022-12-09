7d4a2710b0e54482d20e4cd2a913db4758eec2b4
moses-mugoya <mosesmugoya31@gmail.com>
2022-12-09 16:31:15 +0300

    first commit

---
+++ app/src/main/java/com/example/budgettracker/MainActivity.kt
@@ -0,0 +1,137 @@
+package com.example.budgettracker
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var transactions: List<Transaction>
+    private  lateinit var transactionsAdapter: TransactionAdapter
+    private  lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var db: AppDatabase
+    private lateinit var deletedTransaction: Transaction
+    private lateinit var oldTransactions: List<Transaction>
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        transactions = arrayListOf()
+
+        transactionsAdapter = TransactionAdapter(transactions)
+        linearLayoutManager = LinearLayoutManager(this)
+
+        db = Room.databaseBuilder(this,
+            AppDatabase::class.java,
+            "transactions").build()
+
+        recyclerview.apply {
+            adapter = transactionsAdapter
+            layoutManager = linearLayoutManager
+        }
+
+        //swipe to remove
+         val itemTouchHelper = object: ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT){
+             override fun onMove(
+                 recyclerView: RecyclerView,
+                 viewHolder: RecyclerView.ViewHolder,
+                 target: RecyclerView.ViewHolder
+             ): Boolean {
+                 return false
+             }
+
+             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                 deleteTransaction(transactions[viewHolder.adapterPosition])
+             }
+
+         }
+
+
+        val swipeHelper = ItemTouchHelper(itemTouchHelper)
+        swipeHelper.attachToRecyclerView(recyclerview)
+
+        addBtn.setOnClickListener {
+            val intent = Intent(this, AddTransactionActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun undoDelete(){
+        GlobalScope.launch {
+            db.transactionDao().insertAll(deletedTransaction)
+            transactions =  oldTransactions
+            runOnUiThread {
+                updateDashboard()
+                transactionsAdapter.setData(transactions)
+            }
+        }
+    }
+    private fun showSnackBar(){
+        val view = findViewById<View>(R.id.coordinator)
+        val snackbar = Snackbar.make(view, "Transaction Deleted", Snackbar.LENGTH_LONG)
+        snackbar.setAction("Undo"){
+            undoDelete()
+        }
+            .setActionTextColor(ContextCompat.getColor(this, R.color.red))
+            .setTextColor(ContextCompat.getColor(this, R.color.white))
+            .show()
+    }
+
+
+    private fun deleteTransaction(transaction: Transaction){
+        deletedTransaction = transaction
+        oldTransactions = transactions
+
+        GlobalScope.launch {
+            db.transactionDao().delete(transaction)
+
+            transactions = transactions.filter { it.id != transaction.id }
+
+            runOnUiThread {
+                updateDashboard()
+                transactionsAdapter.setData(transactions)
+                showSnackBar()
+            }
+        }
+
+
+    }
+
+    private fun updateDashboard(){
+        var totalBalance = transactions.sumOf { it.amount }
+        var budgetAmount = transactions.filter { it.amount > 0 }.sumOf { it.amount }
+        var expenseAmount = totalBalance - budgetAmount
+
+        balance.text = "$ %.2f".format(totalBalance)
+        budget.text = "$ %.2f".format(budgetAmount)
+        expense.text = "$ %.2f".format(expenseAmount)
+
+    }
+
+    private fun fetchALl(){
+        GlobalScope.launch {
+            transactions = db.transactionDao().getAll()
+
+            runOnUiThread{
+                updateDashboard()
+                transactionsAdapter.setData(transactions)
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        fetchALl()
+    }
+}
